1dfb28d63e24ea2243ba6166436c1a67cd6b7f40
AlanisYates <yatesalanis@gmail.com>
2019-12-06 20:05:42 -0500

    adding in cards

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var scroll = new SmoothScroll('.topnav [href*="#"]', {
+  speed: 500
+});
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
