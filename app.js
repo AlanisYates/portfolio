d35715e8190330d75c6b4750a732c0742815b255
AlanisYates <yatesalanis@gmail.com>
2019-12-08 01:51:53 -0500

    editing responsive

--- app.js
+++ app.js
@@ -11,3 +11,33 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+// scrollmagic animation
+const flightPath = {
+  curviness: 1.25,
+  autoRotate: true,
+  values: [
+    { x: 100, y: -600 },
+    {x: window.innerWidth, y: 0},
+  ]
+};
+
+const tween = new TimelineLite();
+
+tween.add(
+  TweenLite.to(".circle-3", 1, {
+    bezier: flightPath,
+    ease: Power1.easeInOut
+  })
+);
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".animation",
+  duration: 1000,
+  triggerHook: 0.3
+})
+  .setTween(tween)
+  .setPin(".animation")
+  .addTo(controller);
